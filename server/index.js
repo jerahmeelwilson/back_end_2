06f42ea233cc106b90185e48d9ed0443d13501fe
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-11-02 14:26:04 -0500

    Added  controller.js and index.js

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const {
+    getHouses,
+    createHouse,
+    updateHouse,
+    deleteHouse,
+} = require('./controller.js')
+
+app.get(`/api/houses`,getHouses);
+app.delete(`/api/houses/:id`,deleteHouse);
+app.post('/api/houses',createHouse);
+app.put('/api/houses/:id',updateHouse);
+
+
+
+app.listen(4004,() => console.log(`Server running on port 4004`));
